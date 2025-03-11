ab81414e87d6fc76ad7f79ee746c398fe9b43f61
Jens Kaas Benner <jens@bennerhq.com>
2025-03-11 16:30:51 +0100

    Refactor configuration structure in wclock.py and wclock.yaml; consolidate color settings into a nested dictionary for improved organization and readability

--- wclock.py
+++ wclock.py
@@ -21,20 +21,23 @@ from PyQt5.QtGui import QPainter, QPen, QColor, QGuiApplication
 
 # Function to load configuration from YAML file
 default_config = {
-    'background_color': '#FFFFFF',
-    'hour_mark_color': '#000000',
-    'minute_mark_color': '#000000',
-    'second_hand_color': '#FF0000',
-    'hour_hand_color': '#000000',
-    'minute_hand_color': '#000000',
-    'dial_color': '#FFFFFF00',
-    'date_background_color': "#000000",
-    'date_color': "#FFFFFF",
-    'frameless': True,
-    'always_on_top': True,
-    'tool': True,
+    'colors': {
+        'background': '#FFFFFF',
+        'hour_mark': '#000000',
+        'minute_mark': '#000000',
+        'second_hand': '#FF0000',
+        'hour_hand': '#000000',
+        'minute_hand': '#000000',
+        'dial': '#FFFFFF00',
+        'date_background': "#000000",
+        'date': "#FFFFFF",
+        'date_font': "Courier New"
+    },
 
     'window': {
+        'frameless': True,
+        'always_on_top': True,
+        'tool': True,
         'x': -200,
         'y': -200,
         'width': 200,
@@ -42,8 +45,41 @@ default_config = {
     }
 }
 
-def get_color(config, key: str) -> QColor:
-    value = config.get(key, default_config[key]).strip().lower()
+colors_config = {}
+window_config = {}
+
+def deep_merge(default, override):
+    """
+    Recursively merges override dictionary into default dictionary.
+    """
+    for key, value in override.items():
+        if key in default:
+            if isinstance(default[key], dict) and isinstance(value, dict):
+                deep_merge(default[key], value)
+            else:
+                default[key] = value
+        else:
+            default[key] = value
+    return default
+
+def load_config(yaml_filename : str) -> dict:
+    config = default_config
+
+    try:
+        with open(yaml_filename, 'r') as file:
+            config_yaml = yaml.safe_load(file)
+            config = deep_merge(config, config_yaml)
+    except FileNotFoundError:
+        print(f"Configuration file not found: {yaml_filename}. Using default settings.")
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML file: {e}. Using default settings.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}. Using default settings.")
+
+    return config
+
+def get_qcolor(config, key: str) -> QColor:
+    value = config.get(key, default_config['colors'][key]).strip().lower()
 
     try:
         if value in ["", "none", "transparent"]:
@@ -68,41 +104,12 @@ def get_color(config, key: str) -> QColor:
 
     return QColor(value)
 
-def load_config(yaml_filename : str) -> dict:
-    config = default_config
-
-    try:
-        with open(yaml_filename, 'r') as file:
-            config = yaml.safe_load(file)
-    except FileNotFoundError:
-        print(f"Configuration file not found: {yaml_filename}. Using default settings.")
-    except yaml.YAMLError as e:
-        print(f"Error parsing YAML file: {e}. Using default settings.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}. Using default settings.")
-
-    return {
-        'background_color': get_color(config, 'background_color'),
-        'hour_mark_color': get_color(config, 'hour_mark_color'),
-        'minute_mark_color': get_color(config, 'minute_mark_color'),
-        'second_hand_color': get_color(config, 'second_hand_color'),
-        'hour_hand_color': get_color(config, 'hour_hand_color'),
-        'minute_hand_color': get_color(config, 'minute_hand_color'),
-        'dial_color': get_color(config, 'dial_color'),
-        'date_background_color': get_color(config, 'date_background_color'),
-        'date_color': get_color(config, 'date_color'),
-        'frameless': config.get('frameless', default_config['frameless']),
-        'always_on_top': config.get('always_on_top', default_config['always_on_top']),
-        'tool': config.get('tool', default_config['tool']),
-        'window': config.get('window', default_config['window'])
-    }
-
 class ClockWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update)
-        if config['second_hand_color'].alpha() != 0:
+        if colors_config['second_hand'].alpha() != 0:
             self.timer.start(100)  # Update every 100 milliseconds
         else:
             self.timer.start(1000) # Update every second
@@ -123,38 +130,38 @@ class ClockWidget(QWidget):
         painter.scale(radius / 100.0, radius / 100.0)
 
         # Fill background 
-        if (config['dial_color'].alpha() != 0):
-            painter.setBrush(config['dial_color'])
+        if (colors_config['dial'].alpha() != 0):
+            painter.setBrush(colors_config['dial'])
             painter.setPen(Qt.NoPen)
             painter.drawEllipse(-100, -100, 200, 200)
 
         # Draw hour marks
-        if config['hour_mark_color'].alpha() != 0:
-            painter.setPen(QPen(config['hour_mark_color'], 2))
+        if colors_config['hour_mark'].alpha() != 0:
+            painter.setPen(QPen(colors_config['hour_mark'], 2))
             for i in range(12):
                 painter.drawLine(88, 0, 96, 0)
                 painter.rotate(30)
 
         # Draw minute marks
-        if config['minute_mark_color'].alpha() != 0:
-            painter.setPen(QPen(config['minute_mark_color'], 1))
+        if colors_config['minute_mark'].alpha() != 0:
+            painter.setPen(QPen(colors_config['minute_mark'], 1))
             for i in range(60):
                 if i % 5 != 0:
                     painter.drawLine(92, 0, 96, 0)
                 painter.rotate(6)
 
         # Draw day number at 15:00 o'clock
-        if config['date_background_color'].alpha() != 0:
+        if colors_config['date_background'].alpha() != 0:
             rect = QRect(80, -10, 20, 20)
             today = QDateTime.currentDateTime().toString("dd")
 
             painter.setPen(Qt.NoPen)
-            painter.setBrush(config['date_background_color'])
+            painter.setBrush(colors_config['date_background'])
             painter.save()
             painter.drawRoundedRect(rect, 5, 5)
             painter.restore()
 
-            painter.setPen(QPen(config['date_color'], 1))
+            painter.setPen(QPen(colors_config['date'], 1))
             font = painter.font()
             font.setPointSize(10)
             painter.setFont(font)
@@ -163,24 +170,24 @@ class ClockWidget(QWidget):
             painter.restore()
 
         # Draw hour hand
-        if config['hour_hand_color'].alpha() != 0:
-            painter.setPen(QPen(config['hour_hand_color'], 6, Qt.SolidLine, Qt.RoundCap))
+        if colors_config['hour_hand'].alpha() != 0:
+            painter.setPen(QPen(colors_config['hour_hand'], 6, Qt.SolidLine, Qt.RoundCap))
             painter.save()
             painter.rotate(30 * (current_time.hour() + current_time.minute() / 60.0))
             painter.drawLine(0, 0, 0, -50)
             painter.restore()
 
         # Draw minute hand
-        if config['minute_hand_color'].alpha() != 0:
-            painter.setPen(QPen(config['minute_hand_color'], 4, Qt.SolidLine, Qt.RoundCap))
+        if colors_config['minute_hand'].alpha() != 0:
+            painter.setPen(QPen(colors_config['minute_hand'], 4, Qt.SolidLine, Qt.RoundCap))
             painter.save()
             painter.rotate(6 * (current_time.minute() + current_time.second() / 60.0))
             painter.drawLine(0, 0, 0, -70)
             painter.restore()
 
         # Draw second hand if enabled
-        if config['second_hand_color'].alpha() != 0:
-            painter.setPen(QPen(config['second_hand_color'], 2, Qt.SolidLine, Qt.RoundCap))
+        if colors_config['second_hand'].alpha() != 0:
+            painter.setPen(QPen(colors_config['second_hand'], 2, Qt.SolidLine, Qt.RoundCap))
             painter.save()
             seconds_with_fraction = current_time.second() + current_time.msec() / 1000.0
             painter.rotate(6 * seconds_with_fraction)
@@ -193,8 +200,7 @@ class ClockWindow(QMainWindow):
         self.setWindowTitle("Analog Clock")
         
         screen = QGuiApplication.primaryScreen().geometry()
-        window_config = config['window']
-        
+
         x = int(window_config['x'])
         if x < 0:
             x = screen.width() + x
@@ -207,22 +213,22 @@ class ClockWindow(QMainWindow):
         height = int(window_config['height'])
 
         winFlags = 0
-        if config.get('frameless', True):
+        if window_config['frameless'] == True:
             winFlags |= Qt.FramelessWindowHint
-        if config.get('always_on_top', True):
+        if window_config['always_on_top'] == True:
             winFlags |= Qt.WindowStaysOnTopHint
-        if config.get('tool', True):
+        if window_config['tool'] == True:
             winFlags |= Qt.Tool
         if winFlags != 0:
             self.setWindowFlags(winFlags)
 
         self.setGeometry(x, y, width, height)
-        if config['background_color'].alpha() == 0:
+        if colors_config['background'].alpha() == 0:
             self.setAttribute(Qt.WA_TranslucentBackground)
         else:
             self.setAutoFillBackground(True)
             palette = self.palette()
-            palette.setColor(self.backgroundRole(), config['background_color'])
+            palette.setColor(self.backgroundRole(), colors_config['background'])
             self.setPalette(palette)
 
         self.central_widget = QWidget()
@@ -292,6 +298,10 @@ if __name__ == "__main__":
     print(f"Config file: {config_path}")
 
     config = load_config(config_path)
+    window_config = config['window']
+    colors_config = config['colors']
+    for key, value in colors_config.items():
+        colors_config[key] = get_qcolor(colors_config, key)
 
     app = QApplication(sys.argv)
     win = ClockWindow()
